7413e9035f4231cdbae308d7f2ff4bf02b382810
Xin Zhang <meetdevin.zh@outlook.com>
2021-12-23 16:32:22 +0800

    packaging of MODELLER

---
+++ modeller/alignment.py
@@ -0,0 +1,78 @@
+# encoding: utf-8
+"""
+@author: Xin Zhang
+@contact: zhangxin@szbl.ac.cn
+@time: 12/23/21 2:28 PM
+@desc:
+"""
+from modeller import *
+import sys
+
+
+def align(filepath1, filepath2):
+    id1 = 'receptor'
+    id2 = '7kkl'
+    # log.verbose()
+    env = environ()
+    # env.io.atom_files_directory = './'
+    aln = alignment(env)
+
+    mdl1 = model(env, file=id1, model_segment=('FIRST:A', 'LAST:A'))
+    mdl2 = model(env, file=id2, model_segment=('FIRST:A', 'LAST:F'))
+
+    aln.append_model(mdl1, atom_files=filepath1, align_codes=id1)
+    aln.append_model(mdl2, atom_files=filepath2, align_codes=id2)
+
+    for (weights, write_fit, whole) in (((1., 0., 0., 0., 1., 0.), False, True),
+                                        ((1., 0.5, 1., 1., 1., 0.), False, True),
+                                        ((1., 1., 1., 1., 1., 0.), True, False)):
+        aln.salign(rms_cutoff=3.5, normalize_pp_scores=False,
+                   rr_file='$(LIB)/as1.sim.mat', overhang=30,
+                   gap_penalties_1d=(-450, -50),
+                   gap_penalties_3d=(0, 3), gap_gap_score=0, gap_residue_score=0,
+                   dendrogram_file='salign_out.tree',
+                   alignment_type='tree',  # If 'progresive', the tree is not
+                   # computed and all structues will be
+                   # aligned sequentially to the first
+                   feature_weights=weights,  # For a multiple sequence alignment only
+                   # the first feature needs to be non-zero
+                   improve_alignment=True, fit=True, write_fit=write_fit,
+                   write_whole_pdb=whole, output='ALIGNMENT QUALITY')
+
+    # 输出结构多个文件
+    aln.write(file='complex.pap', alignment_format='PAP')
+    aln.write(file='complex.ali', alignment_format='PIR')
+
+    # 执行对齐
+    aln.salign(rms_cutoff=1.0, normalize_pp_scores=False,
+               rr_file='$(LIB)/as1.sim.mat', overhang=30,
+               gap_penalties_1d=(-450, -50), gap_penalties_3d=(0, 3),
+               gap_gap_score=0, gap_residue_score=0, dendrogram_file='1is3A.tree',
+               alignment_type='progressive', feature_weights=[0] * 6,
+               improve_alignment=False, fit=False, write_fit=True,
+               write_whole_pdb=False, output='QUALITY')
+
+    ########################################
+    aln_block = len(aln)
+    # Read aligned sequence(s) #####################################################
+    aln.append(file='target.ali', align_codes='target')  # 目标序列文件
+
+    # Structure sensitive variable gap penalty sequence-sequence alignment:
+    aln.salign(output='', max_gap_length=20,
+               gap_function=True,  # to use structure-dependent gap penalty
+               alignment_type='PAIRWISE', align_block=aln_block,
+               feature_weights=(1., 0., 0., 0., 0., 0.), overhang=0,
+               gap_penalties_1d=(-450, 0),
+               gap_penalties_2d=(0.35, 1.2, 0.9, 1.2, 0.6, 8.6, 1.2, 0., 0.),
+               similarity_flag=True)
+
+    aln.write(file='ali-mult.ali', alignment_format='PIR')
+    # aln.write(file='ali-mult.pap', alignment_format='PAP')
+
+
+if __name__ == "__main__":
+    filepath1 = sys.argv[1]
+    filepath2 = sys.argv[2]
+
+    align(filepath1, filepath2)
+
--- modeller/model_mult.py
+++ modeller/model_mult.py
@@ -2,32 +2,36 @@ from modeller import *
 from modeller.automodel import *
 from modeller.parallel import *
 import os
+import sys
 
-# num_parallel = 20  # 使用cpu的多线程处理
-# j = job(modeller_path=os.path.join('/home/zhangxin/anaconda3/envs/Bio/lib/modeller-9.25', "bin/modslave.py"))
-# for i in range(num_parallel):
-#     j.append(local_slave())  # 1 Processor
+if __name__ == '__main__':
+    alnfile = sys.argv[1]
+    id1 = sys.argv[2]
+    id2 = sys.argv[3]
+    seq_id = sys.argv[4]
+    # num_parallel = 20  # 使用cpu的多线程处理
+    # j = job(modeller_path=os.path.join('/home/zhangxin/anaconda3/envs/Bio/lib/modeller-9.25', "bin/modslave.py"))
+    # for i in range(num_parallel):
+    #     j.append(local_slave())  # 1 Processor
 
-# env = environ()
-# a = automodel(env, alnfile='ali-mult.ali',
-#               knowns=('6wps_1ab', 'S_10_0001'),
-#               sequence='RBDS309SOPP3')
-# a.starting_model = 1
-# a.ending_model = 10
-# a.use_parallel_job(j)               # Use the job for model building
-# a.make()
+    # env = environ()
+    # a = automodel(env, alnfile='ali-mult.ali',
+    #               knowns=('6wps_1ab', 'S_10_0001'),
+    #               sequence='RBDS309SOPP3')
+    # a.starting_model = 1
+    # a.ending_model = 10
+    # a.use_parallel_job(j)               # Use the job for model building
+    # a.make()
+
+    env = environ()
+    a = automodel(env, alnfile=alnfile,
+                  knowns=(id1, id2), sequence=seq_id)
+    a.starting_model = 1
+    a.ending_model = 10
+
+    # a.loop.starting_model = 1
+    # a.loop.ending_model = 3
+    # a.loop.md_level = refine.fast
+    a.make()
 
 
-#
-# log.verbose()
-env = environ()
-a = loopmodel(env, alnfile='ali-mult.ali',
-              knowns=('6wps_1ab', 'S_10_0001'), sequence='RBDS309SOPP3')
-a.starting_model = 1
-a.ending_model = 10
-
-a.loop.starting_model = 1
-a.loop.ending_model = 2
-a.loop.md_level = refine.fast
-# a.use_parallel_job(j)
-a.make()
