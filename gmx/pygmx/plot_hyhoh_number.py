ff4a093c11d49e89e9f190939d74dfe6584bb245
Xin Zhang <meetdevin.zh@outlook.com>
2022-03-03 10:52:28 +0800

    20220303

--- gmx/pygmx/plot_hyhoh_number.py
+++ gmx/pygmx/plot_hyhoh_number.py
@@ -6,10 +6,15 @@
 @desc:
 """
 import matplotlib.pylab as plt
+import matplotlib
 import numpy as np
+import pandas as pd
 import sys
 import os
+import seaborn as sns
 from scipy.interpolate import make_interp_spline
+matplotlib.rcParams['font.size'] = 20
+matplotlib.rcParams['font.family'] = 'Times New Roman'
 
 
 def read_aw_log(filename):
@@ -32,11 +37,46 @@ def read_aw_log(filename):
     return x, y, n_dict
 
 
+def plot_curves(PDB_ID, Y):
+    fig, ax = plt.subplots()
+    # x_smooth = np.linspace(1000, 10000, num=100)  # 300 represents number of points to make between T.min and T.max
+    # x_smooth = range(1000, 10000, 200)
+    for (id, y) in zip(PDB_ID, Y):
+        ax.plot(time_std, y, label=id)
+
+    ax.set_xlabel('Time (ps)')
+    ax.set_ylabel('Water Number')
+    ax.set_title('Curves of interfacial water number')
+    ax.legend(loc='upper right')
+    plt.xticks(rotation=70)
+    # fig.tight_layout()  # otherwise, the right y-label is slightly clipped
+    plt.show()
+
+
+def plot_heatmap(df):
+    fig, ax = plt.subplots(figsize=(6, 3))
+    fig.set_tight_layout(True)
+    plot = sns.heatmap(df, linewidth=0.5, cmap='coolwarm', annot=False, cbar=True, cbar_kws={'shrink': 0.5},
+                       center=0.5, square=True, xticklabels=np.arange(1000, 10001, 200))
+    for ind, label in enumerate(plot.get_xticklabels()):
+        if ind % 2 == 0:
+            label.set_visible(True)
+        else:
+            label.set_visible(False)
+
+    ax.set_xlabel('Time (ps)')
+    ax.set_ylabel('Antibody-RBD samples')
+    ax.set_title('Heatmap of interfacial water number')
+    # ax.spines['left'].set_visible(True)
+    plt.xticks(rotation=50)
+    plt.show()
+
+
 if __name__ == '__main__':
     work_dir = sys.argv[1]
     PDB_ID = []
     Y = []
-    time_std = range(1000, 10000, 200)
+    time_std = np.arange(1000, 10001, 200)
 
     for path, dir_list, file_list in os.walk(work_dir, topdown=True):
         for filename in file_list:
@@ -57,18 +97,12 @@ if __name__ == '__main__':
                 # X.append(times)
                 Y.append(y_std)
 
-    fig, ax = plt.subplots()
+    # plot_curves(PDB_ID, Y)
 
-    # x_smooth = np.linspace(1000, 10000, num=100)  # 300 represents number of points to make between T.min and T.max
-    # x_smooth = range(1000, 10000, 200)
+    # hyhoh_df = pd.DataFrame(data=Y, index=time_std)
+    plot_heatmap(Y)
+    # hyhoh_df.sort_index()
 
-    for (id, y) in zip(PDB_ID, Y):
-        ax.plot(time_std, y, label=id)
 
-    ax.set_xlabel('Time (ps)')
-    ax.set_ylabel('Water Number')
-    ax.set_title('Curves of interfacial water number')
-    ax.legend(loc='upper right')
-    plt.xticks(rotation=70)
-    # fig.tight_layout()  # otherwise, the right y-label is slightly clipped
-    plt.show()
+
+
