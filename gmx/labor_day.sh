f0d6051057b8a40e7471ff4b83b113333d770e8e
Xin Zhang <MeetDevin.zh@outlook.com>
2022-04-29 19:24:35 +0800

    cnt=$cnt+1

--- gmx/labor_day.sh
+++ gmx/labor_day.sh
@@ -17,29 +17,16 @@ work_dir="/media/xin/Raid0/ACS/gmx/interaction"
 # li_right=(  619        607       194       193       612        638      197       197       195       443       196       640     )
 
 anti_list=("13_7CH5" "14_7E23" "15_7JMO" "16_7K8M" "18_6YM0" "19_6ZER" "20_7C01" "21_7DEO" "22_7MZF" "23_7DPM")
-ri_left=(  1         195       194       1          198       198       196       1         197       1       )
-ri_right=( 424       403       622       428        634       633       628       241       624       444     )
-li_left=(  425       1         1         429        1         1         1         242       1         445     )
-li_right=( 607       194       193       612        197       197       195       443       196       640     )
+ri_left=(   1         195       194       1         198       198       196       1         197       1       )
+ri_right=(  424       403       622       428       634       633       628       241       624       444     )
+li_left=(   425       1         1         429       1         1         1         242       1         445     )
+li_right=(  607       194       193       612       197       197       195       443       196       640     )
+
 
 cnt=0
 
 for anti in "${anti_list[@]}"
 do
-    # for i in $(seq 1 5)
-    # do 
-    #     this_dir=$work_dir/$anti/MD/$i
-    #     cd $this_dir
-    #     mkdir -p hyhoh
-    #     cd hyhoh
-    #     ############## 100 200 10, and 5 frames per ps
-    #     # python $script_dir/main.py -tpr $this_dir/md_0.tpr -xtc $this_dir/md_0.xtc -ri \
-    #     # ${ri_left[$cnt]} ${ri_right[$cnt]} -li ${li_left[$cnt]} ${li_right[$cnt]} -fm normal -rm normal -t 100 200 10 5
-    #     ############## 100 200 10 hyhoh, 5 frames per ps, windows=
-    #     python $script_dir/main.py -tpr $this_dir/md_0.tpr -xtc $this_dir/md_0.xtc -ri \
-    #     ${ri_left[$cnt]} ${ri_right[$cnt]} -li ${li_left[$cnt]} ${li_right[$cnt]} -fm normal -rm hyhoh -t 100 200 10 5
-    # done
-    # cnt=$cnt+1
     this_dir=$work_dir/$anti/MD_10ns
     cd "$this_dir"
     mkdir -p dsthoh
